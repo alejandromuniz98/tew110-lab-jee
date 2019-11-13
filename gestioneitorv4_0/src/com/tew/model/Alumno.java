b119bdb44bc9597460afe988811cca6b6440b4c5
UO258538 <UO258538@GI-AN-B6_10.aulasuo.uniovi.es>
2019-11-13 16:27:45 +0100

    TEW – 7.2 Servicios – Anotación del DTO

--- gestioneitorv4_0/src/com/tew/model/Alumno.java
+++ gestioneitorv4_0/src/com/tew/model/Alumno.java
@@ -1,30 +1,56 @@
 package com.tew.model;
 
-public class Alumno {
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "alumno")
+public class Alumno implements Serializable{
+	
 	private Long id;
 	private String nombre;
 	private String apellidos;
 	private String iduser;
 	private String email;
 
+	
+	
+	public Alumno() {
+	}
+	
+	public Alumno(Long id, String nombre, String apellidos, String iduser, String email) {
+		this.id = id; this.nombre = nombre; this.apellidos = apellidos;
+		this.iduser = iduser; this.email = email;
+	}
+	
+	@XmlElement
 	public String getNombre() {
 		return nombre;
 	}
+	
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
+	@XmlElement
 	public String getApellidos() {
 		return apellidos;
 	}
+	
 	public void setApellidos(String apellidos) {
 		this.apellidos = apellidos;
 	}
+	
+	@XmlElement
 	public String getIduser() {
 		return iduser;
 	}
+	
 	public void setIduser(String iduser) {
 		this.iduser = iduser;
 	}
+	
+	@XmlElement
 	public String getEmail() {
 		return email;
 	}
@@ -35,6 +61,8 @@ public class Alumno {
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
+	@XmlElement
 	public Long getId() {
 		return id;
 	}
